d7b524aee72d508fbdd9700639bc3f198bf92e42
Fabian G. Williams <fabian@adotob.com>
2018-11-28 12:35:13 +0100

    Added a Test Xam.Forms project SpeakerRoom

---
+++ speakerRoomTA1/speakerRoomTA1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace speakerRoomTA1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void firstButton_OnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Notification", "This is all stuff you do already?", "Yes", "No");
+        }
+    }
+}
